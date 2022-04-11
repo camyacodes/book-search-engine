793d42bbea0c69a7387cadf52ee11ce183701561
Camya <camyacodes@gmail.com>
2022-04-10 23:13:18 -0400

    set up server connection bug fix

--- server/server.js
+++ server/server.js
@@ -1,40 +1,54 @@
 const express = require('express');
-// import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
-// import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
+const path = require('path');
 const db = require('./config/connection');
+const routes = require('./routes');
 
-const PORT = process.env.PORT || 3001;
+
+//import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-const startServer = async () => {
-  // create a new Apollo server and pass in our schema data
-  const server = new ApolloServer({ 
-    typeDefs, 
-    resolvers, 
-    // context: authMiddleware 
-  });
+//create new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
 
-  // Start the Apollo server
-  await server.start();
+//integrate our Apollo server with Express application as middleware
+server.applyMiddleware({app});
 
-  // integrate our Apollo server with the Express application as middleware
-  server.applyMiddleware({ app });
-
-  // log where we can go to test our GQL API
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-};
-
-// Initialize the Apollo server
-startServer();
-
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+
+
+app.use(routes); //comment this out in the end
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
   });
+});
+
+process.on('uncaughtException', function(err) {
+  console.log('Caught exception: ' + err);
 });
